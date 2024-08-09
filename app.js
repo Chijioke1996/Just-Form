42656eb200526f635160da78a7874142b418e0cb
unknown <ewuziejoke@gmail.com>
2024-08-09 17:31:56 +0100

    fifth  commit

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 const form = document.querySelector(".form")
-form.addEventListener("submit", (e) => {
+
+form.addEventListener("submit", async (e) => {
     e.preventDefault()
-    window.location.href = "/welcome.html"
+
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+
+    const formData = { name, email }
+    const url = "http://localhost:5000/api/v1/form"
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+
+        const result = await response.json()
+
+        if (response.ok) {
+            window.location.href = "/welcome.html"
+        }
+
+    } catch (error) {
+        console.error("Error:", error)
+        window.location.href = "/index.html"
+    }
+
+
+
 })
